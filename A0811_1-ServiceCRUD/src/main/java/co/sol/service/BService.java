3ec90a855129a8ac0917e0b12dcee0d2c41008d6
harteh <harteh31@gmail.com>
2022-08-11 17:08:23 +0900

    Service CRUD

---
+++ A0811_1-ServiceCRUD/src/main/java/co/sol/service/BService.java
@@ -0,0 +1,21 @@
+package co.sol.service;
+
+import java.util.List;
+
+import co.sol.main.BVO;
+
+public interface BService {
+
+	//글 추가작업
+	Long submit(BVO bo);
+	//특정 게시글 조회
+	BVO get(int bnum);
+	//글 수정
+	//int modify(BVO bo);
+	boolean modify(BVO bo);
+	
+	//글 삭제	
+	boolean del(int bnum);
+	//글 전체 조회
+	List<BVO> getList();
+}
